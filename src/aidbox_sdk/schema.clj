62a0b4d1ce34cc8e6983405fb9261a5acc345b3b
Andrii Demydenko <andrew.demydenko@health-samurai.io>
2024-07-19 11:12:05 +0100

    fix(#7): filter unpacked packages

--- src/aidbox_sdk/schema.clj
+++ src/aidbox_sdk/schema.clj
@@ -3,17 +3,18 @@
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn get-directory-files [path]
+(defn get-packages-from-directory
+  "Returns all packages in the given directory, including files in subdirectories.
+   NOTE: right now it'll filter out all files which do not contains hl7.fhir
+   in their name."
+  [path]
   (->> path
        file-seq
-       (remove #(.isDirectory %))))
-
-(defn fetch-packages [source-path]
-  (->> source-path
-       (get-directory-files)
        (remove #(.isDirectory %))
        ;; FIXME is this really good approach to determine packages?
-       (filter #(str/includes? (.getName %) "hl7.fhir"))))
+       (filter #(str/includes? (.getName %) "hl7.fhir"))
+       ;; FIXME only gzip, but there is no problem to accept unpacked ndjson
+       (filter #(str/ends-with? (.getName %) ".gz"))))
 
 (defn create-gzip-reader [path]
   (-> path
@@ -28,23 +29,33 @@
          (mapv (fn [line]
                  (json/read-str line :key-fn keyword))))))
 
+(defn remove-invalid-schemas [schemas]
+  (remove #(nil? (:package-meta %)) schemas))
+
 (defn merge-duplicates [schemas]
   (->> schemas
        (group-by :url)
        (map (fn [[_url same-url-schemas]]
               (apply merge same-url-schemas)))))
 
+(defn prepare-schemas [schemas]
+  (map #(->> (get-in % [:package-meta :name])
+             (assoc  %  :package))
+       schemas))
+
 
 (defmulti retrieve class)
 
+;; ! According to an example here:
+;; ! https://clojuredocs.org/clojure.java.io/file
+;; ! it's possible to create a File instance from url, which may lead to bugs
 (defmethod retrieve java.io.File
   [source]
-  (->> (fetch-packages source)
+  (->> (get-packages-from-directory source)
        (map parse-package)
        (flatten)
-       (remove #(nil? (:package-meta %)))
-       (map (fn [schema]
-              (assoc schema :package (get-in schema [:package-meta :name]))))
+       (remove-invalid-schemas)
+       (prepare-schemas)
        (merge-duplicates)))
 
 (defmethod retrieve java.net.URL
