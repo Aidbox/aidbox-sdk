a73768dde2f2fefbe2eae3b032073aee5f8a979e
spicyfalafel <285574@niuitmo.ru>
2024-09-19 17:47:51 +0300

    chore: add python lint in ci

--- resources/sdk/python/setup.py
+++ resources/sdk/python/setup.py
@@ -1,18 +1,27 @@
+"""
+setup.py
+"""
+
 from setuptools import setup
 
 setup(
     name="aidbox",
     version="0.0.1",
     description="",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     package_data={"": ["py.typed"]},
     include_package_data=True,
     install_requires=[
         "requests>=2.31.0",
         "types-requests==2.31.0.8",
-        "pydantic[email]==2.0.0",
-        "pydantic_core==2.0.1",
+        "pydantic[email]==2.9.2",
+        # "pydantic_core==2.24.0",
     ],
+    extras_require={
+        'dev': [
+            'pylint'
+        ],
+    },
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
