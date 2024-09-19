25b3ff18b35c4aac1364bd352f5ddc4a1c1f6694
spicyfalafel <285574@niuitmo.ru>
2024-09-19 15:02:51 +0300

    chore: fix lint

--- resources/sdk/typescript/eslint.config.mjs
+++ resources/sdk/typescript/eslint.config.mjs
@@ -1,26 +1,9 @@
 // @ts-check
 
-import js from '@eslint/js';  // ESLint core rules
-import typescript from '@typescript-eslint/eslint-plugin';  // TypeScript ESLint plugin
-import tsParser from '@typescript-eslint/parser';  // TypeScript parser
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-/**
- * @type {import("eslint").Linter.Config}
- */
-export default {
-  parser: tsParser,
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json'  // Ensure tsconfig.json is at this path
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    // Add any custom rules here
-  }
-};
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+);
