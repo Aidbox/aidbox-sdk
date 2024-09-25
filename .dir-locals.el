62493fba8fb8a8ee06d35783f09d93bab7dcb814
Vitaly Kravtsov <mail@krvital.pro>
2024-09-25 12:56:35 +0600

    add emacs shortcut for generating

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,13 @@
-((nil . ((cider-clojure-cli-aliases        . ":dev"))))
+((nil . ((cider-clojure-cli-aliases . ":dev")))
+ (clojure-mode
+  (eval . (defun aidbox-sdk-generate ()
+            (interactive)
+            (let ((language (completing-read "Select the language to generate: "
+                                             '("dotnet" "java" "python" "typescript"))))
+              (cider-interactive-eval
+               (format "(sdk/generate! :%s
+               \"http://localhost:3333/r4/fhir-packages\"
+               {:output-dir \"dist/%s\"
+                :auth-token \"YmFzaWM6c2VjcmV0\"
+                :exit (fn [_] nil)})"
+                       language language)))))))
